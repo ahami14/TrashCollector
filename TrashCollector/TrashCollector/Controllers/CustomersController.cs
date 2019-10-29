9b82654ed30e8403d5358bad746e8d505163db79
ahami14 <ahnikkahamilton14@gmail.com>
2019-10-29 11:03:03 -0500

    Roles added

--- TrashCollector/TrashCollector/Controllers/CustomersController.cs
+++ TrashCollector/TrashCollector/Controllers/CustomersController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrashCollector.Models;
 
 namespace TrashCollector.Controllers
 {
     public class CustomersController : Controller
     {
+        ApplicationDbContext context;
+
         // GET: Customers
         public ActionResult Index()
         {
