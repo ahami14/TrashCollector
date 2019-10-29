02e1dfa9d23903a4f62c3f4e21819a5b2e60d83d
ahami14 <ahnikkahamilton14@gmail.com>
2019-10-29 09:53:24 -0500

    Controllers and databses set up

---
+++ TrashCollector/TrashCollector/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string streetAddress { get; set; }
+        public string cityName { get; set; }
+        public string stateName { get; set; }
+        public int zipCode { get; set; }
+        public double balanceDue { get; set; }
+        public string startDate { get; set; }
+        public string endDate { get; set; }
+        public string confirmedPickup { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
---
+++ TrashCollector/TrashCollector/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public int zipCode { get; set; }
+        public string confirmedPickup { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
